1f5d946719e014843d55ee9e01918b88fa0b3750
Andrea Censi <acensi@ethz.ch>
2020-11-16 00:24:14 +0100

    fix

--- src/duckietown_aido_ros_bridge/__init__.py
+++ src/duckietown_aido_ros_bridge/__init__.py
@@ -6,5 +6,5 @@ logging.basicConfig()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-logger.debug(f"duckietown_aido_ros_bridge version {__version__}")
+logger.debug(f"duckietown_aido_ros_bridge version {__version__} path {__file__}")
 from .interface import run_ros_bridge, run_ros_bridge_main
